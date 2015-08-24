936aaa450d04b57d616a65f04ad4b455e269a146
kurogiri123 <kurogiri123@gmail.com>
2015-08-24 10:26:58 +0900

    Revert "Revert "サーバーとのローカル座標変換""
    This reverts commit 7bc6eb76ce0ecbbae68d7501fcffccd1f3f3d484.

---
+++ MoveKoma.cs
@@ -0,0 +1,145 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text; 
+using System.IO; 
+using System.Net;
+using UnityEngine.UI;
+using MiniJSON;
+using System;
+
+public class MoveKoma : MonoBehaviour {
+
+
+	private static GameObject target;
+	public GameObject Masu;
+	private MoveKoma Control;
+	private Vector3 MasuPosition;
+	private Vector3 Move;
+
+	static string PieceName;
+
+	bool CheckPosition(int posx,int posy){
+		return (posx <= 9 && posx>=1 && posy <=9 && posy >=1);
+	}
+
+	void Start () {
+	}
+	//-----------------------------------------------マスを生成します-----------------------------------------------
+	bool CreateMasu(){
+		int posx = ban.LocalX ((int)MasuPosition.x);
+		int posy = ban.LocalY ((int)MasuPosition.y);
+		if (CheckPosition (posx, posy) == true) {
+			GameObject MasuClone = (GameObject)Instantiate (Masu, transform.localPosition, transform.rotation);
+			MasuClone.transform.SetParent (UnityEngine.Object.FindObjectOfType<Canvas> ().transform);
+			posx = ban.ServerX(posx);
+			posy = ban.ServerY(posy);
+			MasuClone.transform.localPosition = new Vector3(posx,posy,0);
+			Debug.Log("create masu");
+			return true;
+		} else {
+			Debug.Log("create fail");
+			return false;
+		}
+	}
+	//--------------------------------------------マスを生成します------------------------------------------------
+	//--------------------------------------------生成されたマスを消去します----------------------------------------
+	void DestroyMasu(){
+		GameObject[] ObjectMasu = GameObject.FindGameObjectsWithTag ("masu");
+		foreach (GameObject masu in ObjectMasu) {
+			Destroy (masu);
+		}
+	}
+	//--------------------------------------------生成されたマスを消去します----------------------------------------
+
+	//駒の動きとマスの配置
+	//-----------------------------------------------------------味方---------------------------------------------
+	public void OnClickedHu() {
+		target = this.gameObject;
+		DestroyMasu ();
+		MasuPosition = transform.localPosition + new Vector3 (0, 43, 0);
+		CreateMasu ();
+	}
+	public void OnClickedKaku(){
+		target = this.gameObject;
+		DestroyMasu ();
+		for (int i = 1; i<=10; i++) {
+			MasuPosition = transform.localPosition + new Vector3 (43, 43, 0)*i;
+			CreateMasu ();
+			MasuPosition = transform.localPosition + new Vector3 (-43, -43, 0)*i;
+			CreateMasu ();
+			MasuPosition = transform.localPosition + new Vector3 (-43, 43, 0)*i;
+			CreateMasu ();
+			MasuPosition = transform.localPosition + new Vector3 (43, -43, 0)*i;
+			CreateMasu ();
+		}
+	}
+	public void OnClickedKyosya(){
+		target = this.gameObject;
+		DestroyMasu ();
+		for (int i =1; i<=10; i++) {
+			MasuPosition = transform.localPosition + new Vector3 (0, 43, 0) * i;
+			CreateMasu ();
+		}
+	}
+
+	public void moveClicked(){
+		Move = transform.position;
+		target.transform.position = Move;
+		DestroyMasu ();
+	}
+	//----------------------------------------------------------味方-----------------------------------------------
+	//----------------------------------------------------------敵---------------------------------------------
+	public void EnemyOnClickedHu() {
+		target = this.gameObject;
+		DestroyMasu ();
+		MasuPosition = transform.localPosition + new Vector3 (0, -43, 0);
+		CreateMasu ();
+	}
+	public void EnemyOnClickedKaku(){
+		target = this.gameObject;
+		DestroyMasu ();
+		for (int i = 1; i<=10; i++) {
+			MasuPosition = transform.localPosition + new Vector3 (43, 43, 0)*i;
+			CreateMasu ();
+			MasuPosition = transform.localPosition + new Vector3 (-43, -43, 0)*i;
+			CreateMasu ();
+			MasuPosition = transform.localPosition + new Vector3 (-43, 43, 0)*i;
+			CreateMasu ();
+			MasuPosition = transform.localPosition + new Vector3 (43, -43, 0)*i;
+			CreateMasu ();
+		}
+	}
+	public void EnemyOnClickedKyosya(){
+		target = this.gameObject;
+		DestroyMasu ();
+		for (int i =1; i<=10; i++) {
+			MasuPosition = transform.localPosition + new Vector3 (0, -43, 0) * i;
+			CreateMasu ();
+		}
+	}
+
+	public void EnemyMasuClicked(){
+		DestroyMasu ();
+	}
+	//-------------------------------------------------------敵------------------------------------------------
+
+	//-----------------------------------------------------駒の移動制限------------------------------------------------
+	public void PiecesInformation()
+	{
+		string url = Post.ipaddr + Post.Port + "/plays/" + Login.GetSavedPlay ().ToString () +"/pieces";
+		UnityEngine.Events.UnityAction<string> Request = CheckOwner;
+		Post.GetPost ().GET (url, Request);
+	}
+
+	public static void CheckOwner(string json){
+		for (int i =1; i<=40; i++) {
+			PieceName = json [i].ToString ();
+			var piece = (Dictionary<string,object>)json [i];
+			bool CheckOwner = Convert.ToInt32 (piece ["owner"]) == Login.GetSavedUser ();
+			int posx = Convert.ToInt32(piece["posx"]);
+			int posy = Convert.ToInt32(piece["posy"]);
+			ban.SetKomaIdArray(posx,posy,i);
+		}
+	}
+}
---
+++ ban.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ban : MonoBehaviour {
+
+	private static Vector3 LocalStandardPosition = new Vector3(49,183,0);//盤の1*1のglobal position
+	private static Vector3 ServerStandardPosition = new Vector3 (1, 1, 0);//serverのglobal position
+//--------------------------------------------serverとglobal positionの座標変換-------------------------------
+	public static int LocalX(int PLX){//PLX = Pieces Local X
+		int Lx = (int)LocalStandardPosition.x;
+		Lx = Lx - PLX;
+		Lx = (Lx / 43) + 1;
+		return Lx;//Lxはサーバーのx値になっている
+	}
+
+	public static int LocalY(int PLY){//PLY = Pieces Local Y
+		int Ly = (int)LocalStandardPosition.y;
+		Ly = Ly - PLY;
+		Ly = (Ly / 43) + 1;
+		return Ly;
+	}
+
+	public static int ServerX(int PSX){//PSX = Pieces Server X
+		int Sx = (int)ServerStandardPosition.x;
+		Sx = Sx - PSX;
+		Sx = (Sx * 43) + (int)LocalStandardPosition.x;
+		return Sx;
+	}
+
+	public static int ServerY(int PSY){//PSY = Pieces Server Y
+		int Sy = (int)ServerStandardPosition.y;
+		Sy = Sy - PSY;
+		Sy = (Sy * 43) + (int)LocalStandardPosition.y;
+		return Sy;
+	}
+//--------------------------------------------serverとglobal positionの座標変換-------------------------------
+//盤の9*9の配列
+	public static int[,] KomaIdArray = new int [9,9];
+
+	public static void SetKomaIdArray (int posx, int posy, int Id){
+		KomaIdArray [posx - 1] [posy - 1] = Id;
+	}
+}
+
